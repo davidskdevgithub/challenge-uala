7a893f3ab394a224c4ade9d610a4dbfd6ad78b7f
davidsk <davidsk.dev@gmail.com>
2025-04-11 12:44:13 -0300

    feat(filters): add date range picker to filters drawer
    Introduce a date range picker component using react-day-picker and date-fns to allow users to filter transactions by date. This enhances the filtering capabilities of the application by providing a more granular date selection option.

--- src/modules/transactions/filters/components/filters-drawer.tsx
+++ src/modules/transactions/filters/components/filters-drawer.tsx
@@ -11,16 +11,23 @@ import {
 } from '@/components/ui/drawer';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
+import { Calendar } from '@/components/ui/calendar';
 
 import IconFilter from '@/components/icons/icon-filter';
-import { ChevronLeft, X } from 'lucide-react';
+import { ChevronLeft, X, ChevronRight } from 'lucide-react';
 
 import { useFilters } from '../hooks/useFilters';
 import { FilterSection } from './filter-section';
 import { FilterType } from '../filters.types';
 
+import { DateRange } from 'react-day-picker';
+
+const CURRENT_YEAR = new Date().getFullYear();
+
 export const FiltersDrawer = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [testCheckedDate, setTestCheckedDate] = useState(false);
+  const [testDateRange, setTestDateRange] = useState<DateRange>();
 
   const {
     storeFilters,
@@ -168,6 +175,39 @@ export const FiltersDrawer = () => {
                   )}
                 </FilterSection>
               )}
+
+              <FilterSection
+                icon="calendar"
+                title="Fecha"
+                checked={testCheckedDate}
+                onCheckedChange={setTestCheckedDate}
+              >
+                {testCheckedDate && (
+                  <div className="flex flex-col items-end max-w-3xs mx-auto mb-4 pb-4 border rounded-md bg-white">
+                    <Calendar
+                      mode="range"
+                      selected={testDateRange}
+                      onSelect={range => setTestDateRange(range)}
+                      className="w-full"
+                      components={{
+                        IconLeft: () => <ChevronLeft className="h-4 w-4" />,
+                        IconRight: () => <ChevronRight className="h-4 w-4" />,
+                      }}
+                      disabled={{
+                        before: new Date(CURRENT_YEAR, 0, 1),
+                        after: new Date(CURRENT_YEAR, 11, 31),
+                      }}
+                    />
+                    <Button
+                      variant="outline"
+                      onClick={() => setTestDateRange(undefined)}
+                      className="text-primary-blue border-primary-blue hover:bg-primary-blue-light hover:text-primary-blue rounded-3xl mr-4"
+                    >
+                      Borrar
+                    </Button>
+                  </div>
+                )}
+              </FilterSection>
             </div>
           </div>
 
